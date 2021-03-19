7d25c61b00acf34cb0bcd2c9ee334e6569488ebd
ArunTeltia <teltia.arun18@gmail.com>
2021-03-20 00:08:31 +0530

    route change from /download to /

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ import Home from "./views/Home.vue";
 import Device from "./views/Device.vue";
 import Changelog from "./views/Changelog.vue";
 
+// eslint-disable-next-line import/prefer-default-export
 export const routes = [
-  { path: "/download", component: Home, name: "home" },
-  { path: "/download/changelog", component: Changelog, name: "changelog" },
+  { path: "/", component: Home, name: "home" },
+  { path: "/changelog", component: Changelog, name: "changelog" },
   {
-    path: "/download/:codename",
+    path: "/:codename",
     component: Device,
     name: "device",
     children: [
